17e1068cb6f0d9d0187067a3a5645f3b3395b93d
uzbek <uzbekbest0@gmail.com>
2023-07-29 13:07:43 +0500

    first commit

---
+++ sak3app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Sak3AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sak3app'
---
+++ sakapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SakappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sakapp'
